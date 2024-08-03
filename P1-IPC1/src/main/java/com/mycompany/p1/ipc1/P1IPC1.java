52f172d3b2a42eec5d1628949b47e331d94ebd05
G E O <Melvinggs16@gmail.com>
2024-08-02 22:40:19 -0600

    Inicio
    Solo es el inicio del proyecto sin nada más

---
+++ P1-IPC1/src/main/java/com/mycompany/p1/ipc1/P1IPC1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p1.ipc1;
+
+/**
+ *
+ * @author G E O
+ */
+public class P1IPC1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
